270ade69450f6294aae9aaf936c1924322672b83
felixbolefeysot <felix.bolefeysot@gmail.com>
2025-10-02 23:35:51 +0200

    ajout types pour menu

--- test.pas
+++
@@ -1,5 +0,0 @@
-program test;
-
-begin
-   writeln('test');
-end.
---
+++ typesmenu.pas
@@ -0,0 +1,16 @@
+unit typesmenu;
+    
+interface
+    
+    Type TProfil=record
+        nom : string;
+        scores : array[1..1] of integer
+    end;
+
+    Type TListeProfils=record
+        profils : array of TProfil
+    end;
+
+implementation
+     
+end.

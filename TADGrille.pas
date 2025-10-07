9fe4696e497790294280e26b13dfce86321d0425
felixbolefeysot <felix.bolefeysot@gmail.com>
2025-10-07 20:59:12 +0200

    Update TADGrille.pas

--- TADGrille.pas
+++ TADGrille.pas
@@ -8,9 +8,12 @@ Type TGrille = array [1..7,1..6] of integer;
 
 procedure CreerGrilleVide(var g : TGrille);
 function estColonneValide(c : integer; g : TGrille) : boolean;
-procedure PoserJeton(joueur : integer; var g : TGrille );
-{function estGagné(g : TGrille; joueur : Integer) : integer;
-procedure ModifScoreP4(j : integer; var p : TListeProfils);}
+procedure PoserJeton(joueur : integer; var g : TGrille; c,l : integer);
+function estGagne(g : TGrille; joueur,c,l : Integer) : Boolean;
+function verifligne(g : TGrille; joueur,l : Integer) : Boolean;
+function verifcolonne(g : TGrille; joueur,c : Integer) : Boolean;
+function verifdiagonale(g : TGrille; joueur,c,l : Integer) : Boolean;
+procedure ModifScoreP4(joueur : integer; var p : TListeProfils);
 
 implementation
 
@@ -33,8 +36,7 @@ begin
         estColonneValide:=true;
 end;
 
-procedure PoserJeton(joueur : integer; var g : TGrille );
-var c,i : integer;
+procedure PoserJeton(joueur : integer; var g : TGrille ; c,l : integer);
 begin
     writeln('choissisez le numéro de la colonne (1-7) dans laquelle vous voulez poser votre jeton');
     repeat
@@ -42,19 +44,96 @@ begin
         if estColonneValide(c,g)=false then
             writeln('cette colonne est pleine ou n''existe pas, veuillez en choisir une autre');
     until estColonneValide(c,g);
-    i:=6;
-    while (g[c,i]<>0) do
-        i:=i-1;
-    g[c,i]:=joueur;
+    l:=6;
+    while (g[c,l]<>0) do
+        l:=l-1;
+    g[c,l]:=joueur;
     
 end;
 
-{function estGagné(g : TGrille; joueur : Integer) : integer;
-
-procedure ModifScoreP4(j : integer; var p : TListeProfils);
+function estGagne(g : TGrille; joueur,c,l : Integer) : Boolean;
 begin
-    p.profils[j].score[1]=p.profils[j].score[1]+1;
-end;}
+    verifligne(g,joueur,l);
+    verifcolonne(g,joueur,c);
+    verifdiagonale(g,joueur,c,l);
+    if (verifligne(g,joueur,l)=true) or (verifcolonne(g,joueur,c)=true) or (verifdiagonale(g,joueur,c,l)=true) then
+        estGagne:=True
+    else
+        estGagne:=False;
+end;
+
+function verifligne(g : TGrille; joueur,l : Integer) : Boolean;
+var c,count : integer;
+begin
+    count:=0;
+    for c:=1 to 7 do
+        if g[c,l]=joueur then
+            count:=count+1
+        else
+            count:=0;
+    if count>=4 then
+        verifligne:=True
+    else
+        verifligne:=False;
+end;
+
+function verifcolonne(g : TGrille; joueur,c : Integer) : Boolean;
+var l,count : integer;
+begin
+    count:=0;
+    for l:=1 to 6 do
+        if g[c,l]=joueur then
+            count:=count+1
+        else
+            count:=0;
+    if count>=4 then
+        verifcolonne:=True
+    else
+        verifcolonne:=False;
+end;
+
+function verifdiagonale(g : TGrille; joueur,c,l : Integer) : Boolean;
+var dc,dl,count : integer;
+begin
+    verifdiagonale:=False;
+    count:=0;
+    dc:=c;
+    dl:=l;
+    repeat
+        dc:=dc-1;
+        dl:=dl-1;
+    until (dc=1) or (dl=1);
+    repeat
+        if g[dc,dl]=joueur then
+            count:=count+1
+        else
+            count:=0;
+        dc:=dc+1;
+        dl:=dl+1;
+    until (dc=7) or (dl=6) or (count=4);
+    if count=4 then
+        verifdiagonale:=True;
+    dc:=c;
+    dl:=l;
+    repeat
+        dc:=dc+1;
+        dl:=dl+1;
+    until (dc=7) or (dl=6);
+    repeat
+        if g[dc,dl]=joueur then
+            count:=count+1
+        else
+            count:=0;
+        dc:=dc-1;
+        dl:=dl-1;
+    until (dc=1) or (dl=1) or (count=4);
+    if count=4 then
+        verifdiagonale:=True;
+end;
+procedure ModifScoreP4(joueur : integer; var p : TListeProfils);
+begin
+    p.profils[joueur].scores[1]:=p.profils[joueur].scores[1]+1;
+end;
 
 
 end.
